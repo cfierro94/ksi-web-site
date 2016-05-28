80aea4f02df1c58190425f5cfd814cda9e24a46e
cfierro94 <constanza.fierro94@gmail.com>
2016-05-28 17:12:11 +0200

    partenaires mobile

--- www/js/main.js
+++ www/js/main.js
@@ -46,10 +46,20 @@ jQuery(document).ready(function($){
 		return ( mq == 'mobile' ) ? false : true;
 	}
 
+	jQuery.fn.scrollTo = function(elem, speed) { 
+	    $(this).animate({
+	        scrollTop:  $(this).scrollTop() - $(this).offset().top + $(elem).offset().top 
+	    }, speed == undefined ? 1000 : speed); 
+    	return this; 
+	};
+
+	$(".partenaires").scrollTop(20);
+
 	$(document).ready(function(){
 	  $('.slider').slider();
 	});
 	$(document).ready(function(){
       $('.parallax').parallax();
     });
+
 });
