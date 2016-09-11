338d30af00a44f3671573fe49a4862554fc1f9f2
cfierro94 <constanza.fierro94@gmail.com>
2016-09-11 20:51:24 -0300

    Update
    nav hide in scroll, enterprises added

--- www/js/main.js
+++ www/js/main.js
@@ -49,6 +49,47 @@ jQuery(document).ready(function($){
 	$(document).ready(function(){
 	  $('.slider').slider();
 	});
+
+	// Hide Header on on scroll down
+	var didScroll;
+	var lastScrollTop = 0;
+	var delta = 5;
+	var navbarHeight = $('header').outerHeight();
+
+	$(window).scroll(function(event){
+	    didScroll = true;
+	});
+
+	setInterval(function() {
+	    if (didScroll) {
+	        hasScrolled();
+	        didScroll = false;
+	    }
+	}, 250);
+
+	function hasScrolled() {
+	    var st = $(this).scrollTop();
+	    
+	    // Make sure they scroll more than delta
+	    if(Math.abs(lastScrollTop - st) <= delta)
+	        return;
+	    
+	    // If they scrolled down and are past the navbar, add class .nav-up.
+	    // This is necessary so you never see what is "behind" the navbar.
+	    if (st > lastScrollTop && st > navbarHeight){
+	        // Scroll Down
+	        $('header').removeClass('nav-down').addClass('nav-up');
+	    } else {
+	        // Scroll Up
+	        if(st + $(window).height() < $(document).height()) {
+	            $('header').removeClass('nav-up').addClass('nav-down');
+	        }
+	    }
+	    
+	    lastScrollTop = st;
+	}
+
+	/* scroll fire*/
 	var options = [
 		{selector: '#metho-list', offset: 20, callback: function() {
 	      Materialize.showStaggeredList("#metho-list");
@@ -68,6 +109,21 @@ jQuery(document).ready(function($){
 		e.preventDefault();
 		$(this).parent().toggleClass('q-open');
 	})
+
+	// carrousel clients
+	$(document).ready(function() {
+ 
+	  $("#owl-demo").owlCarousel({
+	 
+		      autoPlay: 3000, //Set AutoPlay to 3 seconds
+		 
+		      items : 4,
+		      itemsDesktop : [1199,3],
+		      itemsDesktopSmall : [979,3]
+		 
+		  });
+	 
+	});
     /*
 	//cards domain mobile
     $('.flip-container').on('click', function(e){
